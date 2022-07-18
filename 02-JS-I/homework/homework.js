0b5fd2df4f45b09142829eb0fff63618f65b02e2
Harry Mantilla <Harry@DESKTOP>
2022-07-17 22:40:41 -0500

    six commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -124,19 +124,19 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  
+  return math.pow(num, 2);  
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  
+  return math.pow(num, 3);  
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-  
+  return math.pow(num, exponent); 
 }
 
 function redondearNumero(num) {
